c70915be3474bb2d89e0a2bb9fd33a3b5a261150
AleshaD <shadrov.al@gmail.com>
2021-12-04 22:29:32 +0300

    create countitem model

---
+++ lib/models/count_item.dart
@@ -0,0 +1,15 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'count_item.g.dart';
+
+@JsonSerializable()
+class CountItem {
+  final String id, name;
+  final int value;
+
+  CountItem({required this.id, required this.name, required this.value});
+
+  factory CountItem.fromJson(Map<String, dynamic> json) => _$CountItemFromJson(json);
+
+  Map<String, dynamic> toJson() => _$CountItemToJson(this);
+}
---
+++ lib/models/count_item.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'count_item.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+CountItem _$CountItemFromJson(Map<String, dynamic> json) => CountItem(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      value: json['value'] as int,
+    );
+
+Map<String, dynamic> _$CountItemToJson(CountItem instance) => <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'value': instance.value,
+    };
