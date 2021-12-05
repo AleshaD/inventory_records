78fa67e80938b71ffd98bb4c634f421260a533aa
AleshaD <shadrov.al@gmail.com>
2021-12-05 03:03:23 +0300

    report data

--- lib/screens/report_data/report_data_screen.dart
+++ lib/screens/report_data/report_data_screen.dart
@@ -1,19 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:inventory/models/count_item.dart';
 import 'package:inventory/providers/storage_provider.dart';
 
 class ReportDataScreen extends StatefulWidget {
   final StorageProvider storage;
-  const ReportDataScreen(this.storage, { Key? key }) : super(key: key);
+  const ReportDataScreen(this.storage, {Key? key}) : super(key: key);
 
   @override
   _ReportDataScreenState createState() => _ReportDataScreenState();
 }
 
 class _ReportDataScreenState extends State<ReportDataScreen> {
+  final TextEditingController txtController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Report'),
+    return FutureBuilder(
+      future: widget.storage.fetchCountItems(),
+      builder: (context, AsyncSnapshot<List<CountItem>> snapshot) {
+        if (snapshot.hasData) {
+          txtController.value = TextEditingValue(text: getMdTable(snapshot.data!));
+          return Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              minLines: 1,
+              maxLines: snapshot.data!.length + 4,
+              controller: txtController,
+            ),
+          );
+        } else {
+          return Container();
+        }
+      },
     );
+  }
+
+  String getMdTable(List<CountItem> items) {
+    String table = 'name | count';
+    table = '$table\n:--- | ---:';
+    for (var item in items) {
+      table = '$table\n${item.name} | ${item.value}';
+    }
+    return table;
   }
 }
